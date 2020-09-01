918ecccb70ad1db59f742eee473b0aad985402ce
rajantamang1 <62918494+rajantamang1@users.noreply.github.com>
2020-09-01 07:14:49 -1000

    add github

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ server.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
+server.get("/github", (req, res) => {
+  res.send("Welcome to github");
+});
 server.listen(PORT, () => {
   console.log("server listening");
 });
